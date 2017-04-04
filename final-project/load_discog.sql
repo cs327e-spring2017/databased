b449caf70b41f555fb175a9d4c8c66ed22a9dfdf
AZ-SU\az-su <tolanbien2@gmail.com>
2017-04-03 19:47:35 -0500

    load files created

---
+++ final-project/load_discog.sql
@@ -0,0 +1,10 @@
+/* C:/Users/az-su/Documents/databased/final-project/load_discog.sql */
+set search_path to discog;
+
+
+copy artists from
+'s3://cs327e-final-project-datasets/discog_csv/artists.csv'
+iam_role 'arn:aws:iam::656818381183:role/redshift_s3_role'
+region 'us-east-1'
+csv quote '"' ignoreheader 1 trimblanks compupdate ON
+maxerror 50;
