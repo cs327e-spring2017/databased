c517f1049b564ffb522411c53c463eb7561f71bb
Mac User <macuser@Macs-MacBook-Air-3.local>
2017-04-12 16:27:05 -0500

    more transforms

--- final-project/transform_musicbrainz.sql
+++ final-project/transform_musicbrainz.sql
@@ -1,4 +1,4 @@
-/* \i C:/Users/az-su/Documents/databased/final-project/checkMusicbrainzCharacters.sql */
+﻿/* \i C:/Users/az-su/Documents/databased/final-project/checkMusicbrainzCharacters.sql */
 set search_path to musicbrainz;
 
 select name from artist order by random() limit 50;
@@ -19,17 +19,26 @@ select max(get_utf8_bytes(name)) from musicbrainz.medium_format;
 25
 */
 
-alter table musicbrainz.artist add column cname varchar(311);
-update musicbrainz.artist set cname = name;
-alter table musicbrainz.artist_credit add column cname varchar(1495);
-update musicbrainz.artist_credit set cname = name;
-alter table musicbrainz.release add column cname varchar(865);
-update musicbrainz.release set cname = name;
-alter table musicbrainz.medium_format add column cname varchar(25);
-update musicbrainz.medium_format set cname = name;
+alter table musicbrainz.artist add column cname varchar(311);
+
+update musicbrainz.artist set cname = name;
+
+alter table musicbrainz.artist_credit add column cname varchar(1495);
+
+update musicbrainz.artist_credit set cname = name;
+
+alter table musicbrainz.release add column cname varchar(865);
+
+update musicbrainz.release set cname = name;
+
+alter table musicbrainz.medium_format add column cname varchar(25);
+
+update musicbrainz.medium_format set cname = name;
+
 
 update musicbrainz.artist set cname = initcap(btrim(split_part(cname,
-';' , 1)));
+';' , 1)));
+
 update musicbrainz.artist set cname = initcap(btrim(split_part(cname,
 '/' , 1)));
 update musicbrainz.artist set cname = initcap(btrim(split_part(cname,
@@ -45,4 +54,67 @@ update musicbrainz.artist set cname = initcap(btrim(split_part(cname,
 update musicbrainz.artist set cname = initcap(btrim(split_part(cname,
 'with' , 1)));
 update musicbrainz.artist set cname = initcap(btrim(split_part(cname,
+'Vs.' , 1)));
+
+update musicbrainz.artist_credit set cname = initcap(btrim(split_part(cname,
+';' , 1)));
+
+update musicbrainz.artist_credit set cname = initcap(btrim(split_part(cname,
+'/' , 1)));
+update musicbrainz.artist_credit set cname = initcap(btrim(split_part(cname,
+'(' , 1)));
+update musicbrainz.artist_credit set cname = initcap(btrim(split_part(cname,
+'[' , 1)));
+update musicbrainz.artist_credit set cname = initcap(btrim(split_part(cname,
+':' , 1)));
+update musicbrainz.artist_credit set cname = initcap(btrim(split_part(cname,
+'-' , 1)));
+update musicbrainz.artist_credit set cname = initcap(btrim(split_part(cname,
+'...' , 1)));
+update musicbrainz.artist_credit set cname = initcap(btrim(split_part(cname,
+'with' , 1)));
+update musicbrainz.artist_credit set cname = initcap(btrim(split_part(cname,
+'Vs.' , 1)));
+
+
+update musicbrainz.release set cname = initcap(btrim(split_part(cname,
+';' , 1)));
+
+update musicbrainz.release set cname = initcap(btrim(split_part(cname,
+'/' , 1)));
+update musicbrainz.release set cname = initcap(btrim(split_part(cname,
+'(' , 1)));
+update musicbrainz.release set cname = initcap(btrim(split_part(cname,
+'[' , 1)));
+update musicbrainz.release set cname = initcap(btrim(split_part(cname,
+':' , 1)));
+update musicbrainz.release set cname = initcap(btrim(split_part(cname,
+'-' , 1)));
+update musicbrainz.release set cname = initcap(btrim(split_part(cname,
+'...' , 1)));
+update musicbrainz.release set cname = initcap(btrim(split_part(cname,
+'with' , 1)));
+update musicbrainz.release set cname = initcap(btrim(split_part(cname,
+'Vs.' , 1)));
+
+
+
+update musicbrainz.medium_format set cname = initcap(btrim(split_part(cname,
+';' , 1)));
+
+update musicbrainz.medium_format set cname = initcap(btrim(split_part(cname,
+'/' , 1)));
+update musicbrainz.medium_format set cname = initcap(btrim(split_part(cname,
+'(' , 1)));
+update musicbrainz.medium_format set cname = initcap(btrim(split_part(cname,
+'[' , 1)));
+update musicbrainz.medium_format set cname = initcap(btrim(split_part(cname,
+':' , 1)));
+update musicbrainz.medium_format set cname = initcap(btrim(split_part(cname,
+'-' , 1)));
+update musicbrainz.medium_format set cname = initcap(btrim(split_part(cname,
+'...' , 1)));
+update musicbrainz.medium_format set cname = initcap(btrim(split_part(cname,
+'with' , 1)));
+update musicbrainz.medium_format set cname = initcap(btrim(split_part(cname,
 'Vs.' , 1)));
